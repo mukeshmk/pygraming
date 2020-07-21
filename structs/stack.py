6da8483bfb292926026fa3496c95a642aac9efec
Mukesh A <amukesh.mk@gmail.com>
2020-07-21 10:26:17 +0100

    stack ds using node class

--- structs/stack.py
+++ structs/stack.py
@@ -1,4 +1,5 @@
 # basic stack data structure - https://www.geeksforgeeks.org/stack-data-structure/
+from node import Node
 
 class Stack():
     def __init__(self):
@@ -29,4 +30,53 @@ class Stack():
         for item in self._items:
             if item == element:
                 return True
+        return False
+
+
+class NodeStack():
+    def __init__(self):
+        self._top = None
+
+    def is_empty(self):
+        return self._top is None
+
+    def print(self):
+        if self.is_empty():
+            raise Exception('Stack Empty!')
+
+        cur_node = self._top
+        while cur_node:
+            print(cur_node.get_data())
+            cur_node = cur_node.get_next()
+
+    def push(self, item):
+        new_node = Node(item)
+        if self.is_empty():
+            self._top = new_node
+        else:
+            new_node.set_next(self._top)
+            self._top = new_node
+    
+    def pop(self):
+        if self.is_empty():
+            raise Exception('Stack Empty!')
+
+        top_node = self._top
+        self._top = self._top.get_next()
+        return top_node.get_data()
+    
+    def top(self):
+        if self.is_empty():
+            raise Exception('Stack Empty!')
+        return self._top.get_data()
+
+    def search(self, element):
+        if self.is_empty():
+            raise Exception('Stack Empty!')
+        
+        cur_node = self._top
+        while cur_node:
+            if cur_node.get_data() == element:
+                return True
+            cur_node = cur_node.get_next()
         return False
