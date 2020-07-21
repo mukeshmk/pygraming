4de727a6ff49e669f583a37629c2245834e5d1e6
Mukesh A <amukesh.mk@gmail.com>
2020-07-21 10:29:20 +0100

    updated linkedlist with more features

--- structs/linkedlist.py
+++ structs/linkedlist.py
@@ -31,6 +31,24 @@ class LinkedList():
                     break
                 cur_node = cur_node.get_next()
 
+    def insert_after(self, element_to_be_inserted, insert_after_element):
+        if self.is_empty():
+            raise Exception('LinkedList Empty! Can\'t insert after!')
+
+        if not self.search(insert_after_element):
+            raise Exception('Element doesn\'t exist in LinkedList')
+
+        node = Node(element_to_be_inserted)
+        cur_node = self._head
+
+        while cur_node:
+            if cur_node.get_data() == insert_after_element:
+                temp_node = cur_node.get_next()
+                cur_node.set_next(node)
+                node.set_next(temp_node)
+                break
+            cur_node = cur_node.get_next()
+
     def pop(self):
         if self.is_empty():
             raise Exception('LinkedList Empty!')
@@ -49,6 +67,30 @@ class LinkedList():
                 cur_node = cur_node.get_next()
         return poped_node.get_data()
 
+    def remove(self, element_to_be_removed):
+        if self.is_empty():
+            raise Exception('LinkedList Empty! Can\'t insert after!')
+        if not self.search(element_to_be_removed):
+            raise Exception('Element doesn\'t exist in LinkedList')
+
+        cur_node = self._head
+        poped_node = self._head
+        prev_node = cur_node
+
+        if self._head.get_data() == element_to_be_removed:
+            self._head = self._head.get_next()
+            return poped_node.get_data()
+        
+        cur_node = cur_node.get_next()
+        while cur_node:
+            if cur_node.get_data() == element_to_be_removed:
+                poped_node = cur_node
+                prev_node.set_next(cur_node.get_next())
+                break
+            prev_node = cur_node
+            cur_node = cur_node.get_next()
+        return poped_node.get_data()
+
     def head(self):
         if self.is_empty():
             raise Exception('LinkedList Empty!')
@@ -64,3 +106,20 @@ class LinkedList():
             cur_node = cur_node.get_next()
         return False
 
+
+l = LinkedList()
+l.append('a')
+l.append('b')
+l.append('c')
+# print(l.search('d'))
+# l.print()
+l.insert_after('x', 'b')
+#l.print()
+l.remove('a')
+l.print()
+
+# print(l.pop())
+# print(l.pop())
+# l.append('d')
+# print(l.pop())
+# l.print()
